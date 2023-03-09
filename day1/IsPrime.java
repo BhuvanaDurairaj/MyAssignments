2c54624397d2dba5b5f4fa5e6836a757b2fe1e52
BhuvanaDurairaj <127008502+BhuvanaDurairaj@users.noreply.github.com>
2023-03-09 16:01:12 +0530

    Update IsPrime.java

--- day1/IsPrime.java
+++ day1/IsPrime.java
@@ -4,14 +4,20 @@ public class IsPrime {
 
 	public static void main(String[] args) {
 		int n = 15;
-		
+		boolean bPrime = true;
 		for (int i = 2; i <n-1; i++) {
 			if (n%i == 0) {
-				System.out.println("The given number" + n + "is a prime number" );			
-				}
-		else {
-			System.out.println("The given number" + n + "is a non prime number");
-
-	}
+				System.out.println("The given number" + n + "is a Non prime number" );			
+				bPrime = false;
+				break;
+			}}
+		 if (bPrime) {
+			 System.out.println("The given number" + n + "is a Prime number");
 		}
-}}
+			
+
+
+}
+			
+	}	
+	
